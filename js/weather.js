a12ea57fba178e2875f2068ad55bacfed951c888
Mauro Bonucci <bonucci@users.noreply.github.com>
2016-08-08 00:18:21 +0100

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -3,9 +3,6 @@
 const query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"%s\")";
 const url = "https://query.yahooapis.com/v1/public/yql?format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&q=";
 
-// Search Input and BTN, Make it more abstract
-
-//Module Pattern
 class Weather {
   constructor(options) {
     this.city = options.city;
